13aeeb6c695f4748247c0716f2475c9ed7161e84
chunyuan.wu <chunyuan.wu@dianrong.com>
2017-06-09 18:27:49 +0800

    正在处理目标数据集

--- house-pricing/data_engineering2.py
+++ house-pricing/data_engineering2.py
@@ -6,9 +6,11 @@ import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Imputer
 from sklearn.linear_model import RidgeCV
+from scipy.stats import skew
 
-pd.set_option('display.float_format', lambda x: '%.3f'%x)
+pd.set_option('display.float_format', lambda x: '%.6f'%x)
 
 def cross_validation(model, tX, tY, vX, vY):
     tScores = cross_val_score(model, tX, tY, scoring='neg_mean_squared_error', cv=10)
@@ -20,7 +22,6 @@ def cross_validation(model, tX, tY, vX, vY):
     print('RMSE on Training set:', tRMSE)
     print('RMSE on Validation set:', vRMSE)
     
-    model.fit(tX, tY)
     tPred = model.predict(tX)
     vPred = model.predict(vX)
 
@@ -60,22 +61,57 @@ def cross_validation(model, tX, tY, vX, vY):
 
 #%%
 train = pd.read_csv('house-pricing/data/train.csv')
-trainX = train.select_dtypes(exclude=['object'])
-numericalF = trainX.columns
-trainX = trainX.fillna(0.0)
+#convert categorical
+train = pd.get_dummies(train)
+#impute
+imputer = Imputer(strategy='median')
+train.loc[:, :] = imputer.fit_transform(train.loc[:, :])
+
+trainX = train.drop('SalePrice', axis=1)
 trainY = train['SalePrice']
 
+#log transfrom of the skewed features
+skewness = trainX.apply(lambda x: skew(x))
+skewness = skewness[abs(skewness) > 0.5]
+skewnessF = skewness.index
+trainX[skewnessF] = np.log1p(trainX[skewnessF])
+trainY = np.log1p(trainY)
+
 stdScaler = StandardScaler()
-trainX.loc[:, numericalF] = stdScaler.fit_transform(trainX.loc[:, numericalF])
+trainX.loc[:, :] = stdScaler.fit_transform(trainX.loc[:, :])
 trainY.loc[:] = stdScaler.fit_transform(trainY.loc[:])
 
-#%%
 tX, vX, tY, vY = train_test_split(trainX, trainY, test_size=0.3, random_state=0)
-print('train x:%s'%str(tX.shape))
-print('validation x:%s'%str(vX.shape))
-print('train y:%s'%str(tY.shape))
-print('validation y:%s'%str(vY.shape))
 
-model = RidgeCV(alphas=[0.01, 0.03, 0.06, 0.1, 0.3, 0.6, 1, 3, 6, 10, 30, 60], cv=10)
+model = RidgeCV(alphas=[100, 150, 200, 500, 600, 700], scoring='neg_mean_squared_error')
+model.fit(trainX, trainY)
 
-cross_validation(model, tX, tY, vX, vY)
+print(model.alpha_)
+a = model.alpha_
+
+model = RidgeCV(alphas=[a * .5, a * .55, a * .6, a * .65, a * .7, a * .75, a * .8,
+                        a * .85, a * .9, a * .95, a * 1, a * 1.05, a * 1.1, a * 1.15,
+                        a * 1.2, a * 1.25, a * 1.3, a * 1.35, a * 1.4], scoring='neg_mean_squared_error')
+model.fit(trainX, trainY)
+
+print(model.alpha_)
+# cross_validation(model, tX, tY, vX, vY)
+
+test = pd.read_csv('house-pricing/data/test.csv')
+test = pd.get_dummies(test)
+test.loc[:, :] = imputer.fit_transform(test.loc[:, :])
+
+print(test.isnull().sum())
+test1, _ = test.align(trainX)
+print(trainX.shape)
+print(test1.shape)
+print(test1.isnull().sum())
+
+# skewness = test.apply(lambda x: skew(x))
+# skewness = skewness[abs(skewness) > 0.5]
+# skewnessF = skewness.index
+# test[skewnessF] = np.log1p(test[skewnessF])
+# test.loc[:, :] = stdScaler.fit_transform(test.loc[:, :])
+
+# pred = model.predict(test)
+# pred
