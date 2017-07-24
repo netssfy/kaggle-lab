db88caabf11fadf92ac0cbe777b87b62b5b38b89
chunyuan wu <seigfriedy@gmail.com>
2017-07-24 22:03:39 +0800

    继续细化，非最佳

--- house-pricing/data_engineering3.py
+++ house-pricing/data_engineering3.py
@@ -11,6 +11,8 @@ from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.preprocessing import LabelEncoder
+from scipy.special import boxcox1p
 from xgboost.sklearn import XGBRegressor
 
 rawTrain = pd.read_csv('house-pricing/data/train.csv')
@@ -75,69 +77,85 @@ mx['SaleType'] = mx['SaleType'].fillna(mx['SaleType'].mode()[0])
 
 mx['MSSubClass'] = mx['MSSubClass'].fillna("None")
 
+#将某些数值型的特征转换成类目型
+mx['MSSubClass'] = mx['MSSubClass'].apply(str)
+mx['OverallCond'] = mx['OverallCond'].apply(str)
+mx['YrSold'] = mx['YrSold'].apply(str)
+mx['MoSold'] = mx['MoSold'].apply(str)
+
+#离散变量编码
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond', 
+        'ExterQual', 'ExterCond','HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1', 
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond', 
+        'YrSold', 'MoSold')
+
+for c in cols:
+    lbl = LabelEncoder() 
+    mx[c] = lbl.fit_transform(mx[c].values)
+
+
 #增加特征
-mergedX['TotalBath'] = mergedX['FullBath'] + mergedX['HalfBath']
-mergedX['BathPerBedroom'] = mergedX['TotalBath'] / mergedX['BedroomAbvGr']
-#mergedX['BathPerRoom'].fillna(0)
+mx['TotalSF'] = mx['TotalBsmtSF'] + mx['1stFlrSF'] + mx['2ndFlrSF']
+mx['TotalBath'] = mx['FullBath'] + mx['HalfBath']
+mx['BathPerBedroom'] = mx['TotalBath'] / mx['BedroomAbvGr']
 
-mergedX['KitchenPerBedroom'] = mergedX['KitchenAbvGr'] / mergedX['BedroomAbvGr']
-mergedX['KitchenPerBath'] = mergedX['KitchenAbvGr'] / mergedX['TotalBath']
+mx['KitchenPerBedroom'] = mx['KitchenAbvGr'] / mx['BedroomAbvGr']
+mx['KitchenPerBath'] = mx['KitchenAbvGr'] / mx['TotalBath']
 
-mergedX['GarageCarsPerBedroom'] = mergedX['GarageCars'] / mergedX['BedroomAbvGr']
+mx['GarageCarsPerBedroom'] = mx['GarageCars'] / mx['BedroomAbvGr']
 
-mergedX = mergedX.replace([np.inf, -np.inf], 0.0001)
+mx = mx.replace([np.inf, -np.inf], 0.0001)
+
+#分类特征dummy
+mx = pd.get_dummies(mx)
+
+#对倾斜大的特征做log
+skewness = mx.apply(lambda x: skew(x))
+skewness = skewness[abs(skewness) > 0.75]
+skewnessF = skewness.index
+lam = 0.15
+for feat in skewnessF:
+    mx[feat] = boxcox1p(mx[feat], lam)
 
 #对y做log,使其更正态
 trainY = np.log1p(rawTrainY)
 
-#分类特征dummy
-mergedX = pd.get_dummies(mergedX)
-
-#对NA做处理
-imputer = Imputer(strategy='median')
-mergedX.loc[:, :] = imputer.fit_transform(mergedX.loc[:, :])
-
-#对倾斜大的特征做log
-skewness = mergedX.apply(lambda x: skew(x))
-skewness = skewness[abs(skewness) > 0.5]
-skewnessF = skewness.index
-mergedX[skewnessF] = np.log1p(mergedX[skewnessF])
-
 #做标准化归一化处理
-stdScaler = StandardScaler()
-mergedX.loc[: ,:] = stdScaler.fit_transform(mergedX.loc[:, :])
+# stdScaler = StandardScaler()
+# mx.loc[: ,:] = stdScaler.fit_transform(mx.loc[:, :])
 
 def GetXGBRegressor(X, Y):
     X = X.as_matrix()
 
     print('=' * 10 + 'XGBRegressor' + '=' * 10)
-    etas = [0.0925, 0.093, 0.0935]
-    depths = [10, 12]
-    child_weights = [3, 4, 5]
-    gammas = [0.0341, 0.0342, 0.0343]
-    params = []
-    for a1 in etas:
-        for a2 in depths:
-            for a3 in child_weights:
-                for a4 in gammas:
-                    params.append({ 'eta': a1, 'depth': a2, 'child_weight': a3, 'gamma': a4})
+    # etas = [0.0925, 0.093, 0.0935]
+    # depths = [10, 12]
+    # child_weights = [3, 4, 5]
+    # gammas = [0.0341, 0.0342, 0.0343]
+    # params = []
+    # for a1 in etas:
+    #     for a2 in depths:
+    #         for a3 in child_weights:
+    #             for a4 in gammas:
+    #                 params.append({ 'eta': a1, 'depth': a2, 'child_weight': a3, 'gamma': a4})
 
-    bestScore = 10000000
-    bestParam = None
-    bestModel = None
-    for param in params:
-        print('running ' + str(param))
-        model = XGBRegressor(learning_rate=param['eta'], max_depth=param['depth'], min_child_weight=param['child_weight'], gamma=param['gamma'])
-        scores = cross_val_score(model, X, Y, scoring='neg_mean_squared_error', cv=5)
-        score = np.sqrt(-scores).mean()
+    # bestScore = 10000000
+    # bestParam = None
+    # bestModel = None
+    # for param in params:
+    #     print('running ' + str(param))
+    #     model = XGBRegressor(learning_rate=param['eta'], max_depth=param['depth'], min_child_weight=param['child_weight'], gamma=param['gamma'])
+    #     scores = cross_val_score(model, X, Y, scoring='neg_mean_squared_error', cv=5)
+    #     score = np.sqrt(-scores).mean()
 
-        print('score=%f'%(score))
-        if score < bestScore:
-            bestScore = score
-            bestParam = param
-            bestModel = model
+    #     print('score=%f'%(score))
+    #     if score < bestScore:
+    #         bestScore = score
+    #         bestParam = param
+    #         bestModel = model
     
-    # bestParam = {'child_weight': 4, 'depth': 10, 'eta': 0.093, 'gamma': 0.0341}
+    bestParam = {'child_weight': 4, 'depth': 10, 'eta': 0.093, 'gamma': 0.0341}
     print('best param = ' + str(bestParam))
     model = XGBRegressor(learning_rate=bestParam['eta'], max_depth=bestParam['depth'], min_child_weight=bestParam['child_weight'], gamma=bestParam['gamma'])
     model.fit(X, Y)
@@ -165,10 +183,10 @@ def GetRandomForestRegressor(X, Y):
     bestModel.fit(X, Y)
     return bestModel
 
-print(mergedX.shape)
+print(mx.shape)
 len = rawTrainX.shape[0]
-trainX = mergedX[:len]
-testX = mergedX[len:]
+trainX = mx[:len]
+testX = mx[len:]
 
 model = GetXGBRegressor(trainX, trainY)
 
--- house-pricing/playground.py
+++ house-pricing/playground.py
@@ -5,7 +5,20 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('house-pricing/data/train.csv')
-
+all_data = pd.read_csv('house-pricing/data/train.csv')
+print all_data['FireplaceQu'][0:5]
+print all_data['BsmtQual'][0:5]
 #%%
-type(data['MSZoning'].mode())
+from sklearn.preprocessing import LabelEncoder
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond', 
+        'ExterQual', 'ExterCond','HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1', 
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond', 
+        'YrSold', 'MoSold')
+# process columns, apply LabelEncoder to categorical features
+for c in cols:
+    lbl = LabelEncoder() 
+    all_data[c] = lbl.fit_transform(all_data[c].values)
+
+print all_data['FireplaceQu'][0:5]
+print all_data['BsmtQual'][0:5]
